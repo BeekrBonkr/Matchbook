17f8cf0b87a5f41f0a2fe6c2ab25914060ceb5b7
BeekrBonkr <86544010+BeekrBonkr@users.noreply.github.com>
2026-02-05 05:46:49 -0500

    Move plugin folder to inside mbedwars addons folder

--- src/main/java/com/slg/matchbook/MatchbookPlugin.java
+++ src/main/java/com/slg/matchbook/MatchbookPlugin.java
@@ -4,20 +4,35 @@ import de.marcely.bedwars.api.BedwarsAPI;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.net.URI;
+
 public final class MatchbookPlugin extends JavaPlugin {
 
     private MatchStorage storage;
     private MatchbookListener listener;
 
+    // NEW: custom data folder under MBedwars/add-ons/
+    private File addonDataFolder;
+
     public MatchbookListener getListener() {
         return listener;
     }
 
+    public File getAddonDataFolder() {
+        return addonDataFolder != null ? addonDataFolder : getDataFolder();
+    }
+
     @Override
     public void onEnable() {
+        this.addonDataFolder = resolveAddonDataFolder();
+        if (!addonDataFolder.exists()) {
+            //noinspection ResultOfMethodCallIgnored
+            addonDataFolder.mkdirs();
+        }
+
         this.storage = new MatchStorage(this);
 
-        // MBedwars API provides an onReady hook; use it so we don't race startup.
         BedwarsAPI.onReady(() -> {
             this.listener = new MatchbookListener(this, storage);
             Bukkit.getPluginManager().registerEvents(listener, this);
@@ -29,7 +44,7 @@ public final class MatchbookPlugin extends JavaPlugin {
                 getLogger().info("Registered PlaceholderAPI expansion: matchbook");
             }
 
-            getLogger().info("Matchbook enabled (MBedwars API ready).");
+            getLogger().info("Matchbook enabled. Data folder: " + getAddonDataFolder().getAbsolutePath());
         });
     }
 
@@ -38,6 +53,25 @@ public final class MatchbookPlugin extends JavaPlugin {
         if (listener != null) {
             listener.flushAll("plugin-disable");
         }
-        getLogger().info("Matchbook disabled.");
+    }
+
+    /**
+     * Resolve: <jar_dir>/Matchbook
+     * If jar is in plugins/MBedwars/add-ons/, data becomes:
+     *   plugins/MBedwars/add-ons/Matchbook/
+     */
+    private File resolveAddonDataFolder() {
+        try {
+            URI uri = getClass().getProtectionDomain().getCodeSource().getLocation().toURI();
+            File jarFile = new File(uri);
+            File jarDir = jarFile.getParentFile(); // should be .../plugins/MBedwars/add-ons
+            if (jarDir != null && jarDir.isDirectory()) {
+                return new File(jarDir, "Matchbook");
+            }
+        } catch (Exception ignored) {
+        }
+
+        // fallback: default Bukkit folder
+        return getDataFolder();
     }
 }
